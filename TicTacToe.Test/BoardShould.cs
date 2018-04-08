a995404a0a3ba2a8f90dedc65fd1d16e0b982533
jordan elley <jordan.elley@myob.com>
2018-04-09 10:38:16 +1200

    created blank state test for board

---
+++ TicTacToe.Test/BoardShould.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace TicTacToe.Test
+{
+    public class BoardShould
+    {
+        [Fact]
+        public void HaveAnInitialBlankState()
+        {
+            var board = new Board();
+            Assert.Equal(board.Player1Moves,new List<Move>());
+            Assert.Equal(board.Player2Moves, new List<Move>());
+            Assert.Equal(board.AllMoves, new List<Move>());
+            
+        }
+
+    }
+
+    public class Board
+    {
+        public List<Move> Player1Moves { get; set; }
+        public List<Move> Player2Moves { get; set; }
+        public List<Move> AllMoves { get; set; }
+    }
+}
