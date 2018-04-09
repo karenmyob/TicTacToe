ec24528ca72aea0c98db41524275ae1153d6964e
jordan elley <jordan.elley@myob.com>
2018-04-09 13:31:08 +1200

    wrote a test to identify a diagonal win

--- TicTacToe.Test/WinCheckerShould.cs
+++ TicTacToe.Test/WinCheckerShould.cs
@@ -6,13 +6,25 @@ namespace TicTacToe.Test
     public class WinCheckerShould
     {
         [Fact]
-        public void IndentifyAWin()
+        public void IndentifyANonWin()
         {
             var winChecker = new WinChecker();
             var move1 = new Move(1,1);
             var move2 = new Move(2,1);
             var moves = new List<Move>{move1,move2};
             bool result = winChecker.DidWin(moves);
+            Assert.False(result);
+        }
+        [Fact]
+        public void IndentifyARightToLeftDiagonalWin()
+        {
+            var winChecker = new WinChecker();
+            var move1 = new Move(1,1);
+            var move2 = new Move(2,2);
+            var move3 = new Move(2,2);
+            var moves = new List<Move>{move1,move2,move3};
+            bool result = winChecker.DidWin(moves);
+            Assert.True(result);
         }
     }
 
@@ -20,7 +32,7 @@ namespace TicTacToe.Test
     {
         public bool DidWin(List<Move> moves)
         {
-            throw new System.NotImplementedException();
+            return false;
         }
     }
 }
