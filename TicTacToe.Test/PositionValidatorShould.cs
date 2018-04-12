1ab152708265448ca3ed19005f79c3d9e7e968fd
jordan elley <jordan.elley@myob.com>
2018-04-12 16:21:03 +1200

    Wrote a test for move validator to decline invalid postions

--- TicTacToe.Test/PositionValidatorShould.cs
+++ TicTacToe.Test/PositionValidatorShould.cs
@@ -15,5 +15,17 @@ namespace TicTacToe.Test
             var result = positionValidator.IsValid(input);
             Assert.True(result);
         }
+        
+        [Theory]
+        [InlineData("31,1")]
+        [InlineData("1,2.")]
+        [InlineData("qq")]
+        [InlineData("4,1")]
+        public void DeclineInvalidPostions(string input)
+        {
+            var positionValidator = new PositionValidator();
+            var result = positionValidator.IsValid(input);
+            Assert.False(result);
+        }
     }
 }
