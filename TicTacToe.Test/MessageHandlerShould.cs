9d0f3a8de4520fbe961c912cf1ce34ec38b0701c
jordan elley <jordan.elley@myob.com>
2018-04-13 10:29:47 +1200

    Created Get play instruction messsage method and test also added an initialisation in the board constuctor

--- TicTacToe.Test/MessageHandlerShould.cs
+++ TicTacToe.Test/MessageHandlerShould.cs
@@ -8,17 +8,47 @@ namespace TicTacToe.Test
         public void ReturnWelcomeMessage()
         {
             var messageHandler = new MessageHandler();
-            string result = messageHandler.GetWelcomeMessage();
-            Assert.Equal("Welcome to Tic Tac Toe!",result);
+            var board = new Board();
+            
+            string result = messageHandler.GetWelcomeMessage(board);           
+            var boardRenderer = new BoardRenderer(board);
+            
+            Assert.Equal("Welcome to Tic Tac Toe!\n\nHere's the current board:\n\n"+boardRenderer.ToText(),result);
+        }
+        
+        [Fact]
+        public void ReturnInstructionWithPlayerName()
+        {
+            var messageHandler = new MessageHandler();
+            string result = messageHandler.GetInstruction("X");
+            Assert.Equal("Player 1 enter a coord x,y to place your X or enter 'q' to give up: ",result);
+        }
 
+        [Fact]
+        public void ReturnAcceptedMoveMessage()
+        {
+            
         }
     }
 
     public class MessageHandler
     {
-        public string GetWelcomeMessage()
+        public string GetWelcomeMessage(Board board)
         {
-            throw new System.NotImplementedException();
+            var boardRenderer = new BoardRenderer(board);
+            return "Welcome to Tic Tac Toe!\n\nHere's the current board:\n\n"+boardRenderer.ToText();
+        }
+
+        public string GetInstruction(string symbol)
+        {
+            var player = "Player ";
+            
+            if (symbol == "X")
+                player += "1";
+            if(symbol == "O")
+                player += "2";
+
+            return player + " enter a coord x,y to place your X or enter 'q' to give up: ";
         }
     }
 }
