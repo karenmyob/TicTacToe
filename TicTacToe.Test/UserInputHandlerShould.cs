45261d2c98b721ebc30c51b33ab58b053983f3a1
jordan elley <jordan.elley@myob.com>
2018-04-12 15:11:09 +1200

    created test for InputHandler

--- TicTacToe.Test/UserInputHandlerShould.cs
+++ TicTacToe.Test/UserInputHandlerShould.cs
@@ -1,12 +1,26 @@
 using System.Runtime.InteropServices.ComTypes;
+using Xunit;
 
 namespace TicTacToe.Test
 {
     public class UserInputHandlerShould
     {
-        public void GetInputFromUser()
+        public void GetMoveFromUser()
         {
+            InputReader inputReader;
+            InputValidator inputValidator;
             
+            var inputHandler = new UserInputHandler();
+            var result = inputHandler.GetInput(inputReader,inputValidator)
+            Assert.True(result);
+        }
+    }
+
+    public class UserInputHandler
+    {
+        public bool GetInput(InputReader inputReader, InputValidator inputValidator)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
