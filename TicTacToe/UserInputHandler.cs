31b3cd4a358b8a302fa765877273c7614cdf75ea
jordan elley <jordan.elley@myob.com>
2018-04-14 11:48:28 +1200

    made the inputHandler accept a dictionary of validators and handlers

--- TicTacToe/UserInputHandler.cs
+++ TicTacToe/UserInputHandler.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace TicTacToe
 {
     public class UserInputHandler
     {
-        public void GetInput(InputReader inputReader, InputValidator inputValidator, InputHandler inputHandler, Board board)
+        //public void GetInput(InputReader inputReader, InputValidator inputValidator, InputHandler inputHandler, Board board)
+        public void GetInput(InputReader inputReader, Dictionary<InputValidator,InputHandler>  executionHandler, Board board)
         {
             var input = inputReader.ReadInput();
             if (inputValidator.IsValid(input))
